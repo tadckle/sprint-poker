43615cf1bfc634e11c119c869f79e63f109bbad8
truejasonfans <472408570@qq.com>
2018-11-22 00:12:18 +0800

    allow empty login name temporarily.

--- sprint-poker-springboot/src/main/java/org/zhxie/sprinpoker/config/STOMPConnectEventListener.java
+++ sprint-poker-springboot/src/main/java/org/zhxie/sprinpoker/config/STOMPConnectEventListener.java
@@ -6,6 +6,8 @@ import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.web.socket.messaging.SessionConnectEvent;
 import org.zhxie.sprinpoker.repository.SocketSessionRegistry;
 
+import java.util.List;
+
 public class STOMPConnectEventListener implements ApplicationListener<SessionConnectEvent> {
 
     @Autowired
@@ -15,7 +17,13 @@ public class STOMPConnectEventListener implements ApplicationListener<SessionCon
     public void onApplicationEvent(SessionConnectEvent event) {
         StompHeaderAccessor sha = StompHeaderAccessor.wrap(event.getMessage());
         //login get from browser
-        String agentId = sha.getNativeHeader("login").get(0);
+        final List<String> login = sha.getNativeHeader("login");
+        String agentId;
+        if (login != null && !login.isEmpty()) {
+            agentId = login.get(0);
+        } else {
+            agentId = "";
+        }
         String sessionId = sha.getSessionId();
         webAgentSessionRegistry.registerSessionId(agentId,sessionId);
     }
