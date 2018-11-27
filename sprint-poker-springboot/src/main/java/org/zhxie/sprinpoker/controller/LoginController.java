b843988763968d6d2c44ec79ad397afbceb8c6b7
truejasonfans <472408570@qq.com>
2018-11-27 23:46:35 +0800

    fix spring boot 支持跨域.

--- sprint-poker-springboot/src/main/java/org/zhxie/sprinpoker/controller/LoginController.java
+++ sprint-poker-springboot/src/main/java/org/zhxie/sprinpoker/controller/LoginController.java
@@ -14,8 +14,8 @@ public class LoginController {
 
     @MessageMapping("/login")
     @SendTo("/pocker/login")
-    public Boolean login(String player) {
-        System.out.println(String.format("%s is logining", player));
+    public Boolean login(Player player) {
+        System.out.println(String.format("%s is logining", player.getName()));
         return true;
     }
 
