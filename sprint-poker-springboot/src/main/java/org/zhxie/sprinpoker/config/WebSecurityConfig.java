77eafc6b1a6d8300ce437232a90f964922d8bdb0
naijgnay <naijgnay@163.com>
2019-01-07 21:21:00 +0800

    add user service test

--- sprint-poker-springboot/src/main/java/org/zhxie/sprinpoker/config/WebSecurityConfig.java
+++ sprint-poker-springboot/src/main/java/org/zhxie/sprinpoker/config/WebSecurityConfig.java
@@ -1,9 +1,11 @@
 package org.zhxie.sprinpoker.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  * Created by jianyang on 1/7/2019.
@@ -20,4 +22,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and().csrf().disable();
     }
+
+    @Bean
+    public BCryptPasswordEncoder bcryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
 }
---
+++ sprint-poker-springboot/src/test/java/org.zhxie.sprinpoker/UserServiceTest.java
@@ -0,0 +1,43 @@
+package org.zhxie.sprinpoker;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.zhxie.sprinpoker.domain.User;
+import org.zhxie.sprinpoker.service.UserService;
+
+/**
+ * Created by jianyang on 2019/1/7.
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserServiceTest {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private BCryptPasswordEncoder encoder;
+
+    @Test
+    public void testSave() {
+        User user = new User();
+        user.setUserName("jianyang");
+        user.setPassword("123");
+        user.setEmail("jian.yang@asml.com");
+        if (userService.findByUserName("jianyang") == null) {
+            User savedUser = userService.save(user);
+            Assert.assertEquals(user.getUserName(), savedUser.getUserName());
+        }
+    }
+
+    @Test
+    public void testFindByUserName() {
+        User user = userService.findByUserName("jianyang");
+        Assert.assertTrue(encoder.matches("123",user.getPassword()));
+    }
+
+}
