c842de71cddf63e66764627f68d8b740c48a2ca0
naijgnay <naijgnay@163.com>
2019-01-10 20:21:39 +0800

    fix JWT parse token bug

--- sprint-poker-springboot/src/main/java/org/zhxie/sprinpoker/interceptor/JwtInterceptor.java
+++ sprint-poker-springboot/src/main/java/org/zhxie/sprinpoker/interceptor/JwtInterceptor.java
@@ -22,8 +22,9 @@ public class JwtInterceptor extends HandlerInterceptorAdapter {
     public boolean preHandle(HttpServletRequest request,
                              HttpServletResponse response, Object handler) throws Exception {
         System.out.println("经过了拦截器");
-        final String token = request.getHeader("token");
-        if (token != null) {
+        final String authHeader = request.getHeader("Authorization");
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            final String token = authHeader.substring(7); // The part after "Bearer "
             Claims claims = jwtUtil.parseJWT(token);
             if (claims != null) {
                 if ("admin".equals(claims.get("roles"))) {//如果是管理员
