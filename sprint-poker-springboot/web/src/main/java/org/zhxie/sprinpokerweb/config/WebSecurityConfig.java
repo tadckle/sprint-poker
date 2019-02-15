1186ef3ece4acc64c2122309df4e54a4c0ac2cb8
truejasonfans <472408570@qq.com>
2019-02-15 23:50:17 +0800

    add the static resources and fix the static resources blocked by InterceptorConfig.

--- sprint-poker-springboot/web/src/main/java/org/zhxie/sprinpokerweb/config/WebSecurityConfig.java
+++ sprint-poker-springboot/web/src/main/java/org/zhxie/sprinpokerweb/config/WebSecurityConfig.java
@@ -1,15 +1,19 @@
 package org.zhxie.sprinpokerweb.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 import org.zhxie.sprinpokerweb.service.CustomUserDetailsService;
+import org.zhxie.sprinpokerweb.service.LoginSuccessHandler;
 
 /**
  * Created by jianyang on 1/7/2019.
@@ -17,30 +21,31 @@ import org.zhxie.sprinpokerweb.service.CustomUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
+@EnableConfigurationProperties(SecuritySettings.class)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Autowired
   private CustomUserDetailsService customUserDetailsService;
 
+  @Autowired
+  private SecuritySettings settings;
+
   @Override
-  protected void configure(HttpSecurity httpSecurity) throws Exception {
+  protected void configure(HttpSecurity http) throws Exception {
     /**
      * Only / ,/home, /login are allowed to directly visit
      * others routes visit need be authenticated
      *
      *  Disable the CORS
      */
-    httpSecurity
-            .authorizeRequests()
+    http.formLogin().loginPage("/login").permitAll().successHandler(loginSuccessHandler())
+            .and().authorizeRequests()
             .antMatchers("/images/**", "/checkcode", "/scripts/**", "/styles/**").permitAll()
-            .anyRequest().authenticated().and()
-            .formLogin()
-              .loginPage("/login").
-              permitAll()
-            .and()
-            .logout()
-              .permitAll()
-            .and().csrf().disable();
+            .anyRequest().authenticated()
+            .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER)
+            .and().logout().logoutSuccessUrl(settings.getLogoutsuccssurl())
+            .and().exceptionHandling().accessDeniedPage(settings.getDeniedpage())
+            .and().rememberMe().tokenValiditySeconds(86400);
   }
 
   @Override
@@ -50,6 +55,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     auth.eraseCredentials(false);
   }
 
+//  @Bean
+//  public JdbcTokenRepositoryImpl tokenRepository(){
+//    JdbcTokenRepositoryImpl jtr = new JdbcTokenRepositoryImpl();
+//    return jtr;
+//  }
+
   @Bean
   public BCryptPasswordEncoder bcryptPasswordEncoder() {
     return new BCryptPasswordEncoder();
@@ -58,6 +69,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected UserDetailsService userDetailsService() {
     return super.userDetailsService();
+  }
+
+  @Bean
+  public LoginSuccessHandler loginSuccessHandler(){
+    return new LoginSuccessHandler();
   }
 
 }
