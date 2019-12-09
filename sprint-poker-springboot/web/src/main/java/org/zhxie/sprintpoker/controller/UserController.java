4c05d2d4a3c6c1a59f72c0e6e6dfc74e36a801c0
zhxie <zhong-wei.xie@asml.com>
2019-12-09 18:21:24 +0800

    moving the isOwner to roomOwner

--- sprint-poker-springboot/web/src/main/java/org/zhxie/sprintpoker/controller/UserController.java
+++ sprint-poker-springboot/web/src/main/java/org/zhxie/sprintpoker/controller/UserController.java
@@ -1,6 +1,7 @@
 package org.zhxie.sprintpoker.controller;
 
 import com.google.common.collect.Maps;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.zhxie.sprintpoker.service.UserService;
 import org.zhxie.sprintpoker.util.JwtUtil;
 
+import java.security.Principal;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -20,28 +23,34 @@ import java.util.Map;
 @Controller
 public class UserController {
 
-  @Autowired
-  private UserService userService;
+    @Autowired
+    private UserService userService;
 
 
-  @RequestMapping(value = "/user/regist", method = RequestMethod.POST, produces = "application/json")
-  public @ResponseBody
-  ResponseResult regist(@RequestBody UserDTO user) {
-    User findUser = userService.findByUserName(user.getUserName());
-    if (findUser != null) {
-      return new ResponseResult(ResponseResult.REGIST_ERROR, "用户名已经被注册");
+    @RequestMapping(value = "/user/regist", method = RequestMethod.POST, produces = "application/json")
+    public @ResponseBody ResponseResult regist(@RequestBody UserDTO user) {
+        User findUser = userService.findByUserName(user.getUserName());
+        if (findUser != null) {
+            return new ResponseResult(ResponseResult.REGIST_ERROR, "用户名已经被注册");
+        }
+        userService.save(user);
+        return new ResponseResult(ResponseResult.SUCCESS, "注册成功");
     }
-    userService.save(user);
-    return new ResponseResult(ResponseResult.SUCCESS, "注册成功");
 
-  }
-
-  @RequestMapping("/login*")
-  public String login(@RequestParam Map<String, String> allParams, ModelMap map) {
-    if (allParams.containsKey("msg")) {
-      map.addAttribute("msg", "密码错误或用户名不存在");
+    @RequestMapping("/login*")
+    public String login(@RequestParam Map<String, String> allParams, ModelMap map) {
+        if (allParams.containsKey("msg")) {
+            map.addAttribute("msg", "密码错误或用户名不存在");
+        }
+        return "login";
     }
-    return "login";
-  }
+
+    @RequestMapping(value = "/api/users/whoAmI", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public Map whoAmI(Principal user) {
+        Map<String, String> nameMap = new HashMap<>();
+        nameMap.put("userName", user.getName());
+        return nameMap;
+    }
 
 }
